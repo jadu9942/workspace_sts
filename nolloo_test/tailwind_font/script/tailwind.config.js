f6e8ac82ef49063593da4046cad402f99b71887a
jadu9942 <jadu9942@gmail.com>
2024-04-08 09:23:01 +0900

    0408

---
+++ nolloo_test/tailwind_font/script/tailwind.config.js
@@ -0,0 +1,12 @@
+// config.js
+
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                'aaa': ['Noto Sans KR', 'Arial', 'sans-serif'],
+                'bbb': ["Single Day", 'cursive']
+            }
+        }
+    }
+} 
